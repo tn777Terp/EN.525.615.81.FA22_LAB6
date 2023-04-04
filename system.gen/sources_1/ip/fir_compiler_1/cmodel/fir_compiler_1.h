436e0c6c88c48279e2ba5097366d58571367ef13
Tin Nguyen <tn777@terpmail.umd.edu>
2023-04-04 01:44:48 -0400

    First commit

---
+++ system.gen/sources_1/ip/fir_compiler_1/cmodel/fir_compiler_1.h
@@ -0,0 +1,115 @@
+
+//------------------------------------------------------------------------------
+// (c) Copyright 2014 Xilinx, Inc. All rights reserved.
+//
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.
+//
+// DISCLAIMER
+// This disclaimer is not a license and does not grant any
+// rights to the materials distributed herewith. Except as
+// otherwise provided in a valid license issued to you by
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+// (2) Xilinx shall not be liable (whether in contract or tort,
+// including negligence, or under any other theory of
+// liability) for any loss or damage of any kind or nature
+// related to, arising under or in connection with these
+// materials, including for any direct, or any indirect,
+// special, incidental, or consequential loss or damage
+// (including loss of data, profits, goodwill, or any type of
+// loss or damage suffered as a result of any action brought
+// by a third party) even if such damage or loss was
+// reasonably foreseeable or Xilinx had been advised of the
+// possibility of the same.
+//
+// CRITICAL APPLICATIONS
+// Xilinx products are not designed or intended to be fail-
+// safe, or for use in any application requiring fail-safe
+// performance, such as life-support or safety devices or
+// systems, Class III medical devices, nuclear facilities,
+// applications related to the deployment of airbags, or any
+// other applications that could lead to death, personal
+// injury, or severe property or environmental damage
+// (individually and collectively, "Critical
+// Applications"). Customer assumes the sole risk and
+// liability of any use of Xilinx products in Critical
+// Applications, subject only to applicable laws and
+// regulations governing limitations on product liability.
+//
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+// PART OF THIS FILE AT ALL TIMES.
+//------------------------------------------------------------------------------ 
+//
+// C Model configuration for the "fir_compiler_1" instance.
+//
+//------------------------------------------------------------------------------
+//
+// coefficients: 3,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-3,-3,-3,-3,-4,-4,-4,-5,-5,-5,-6,-6,-6,-7,-7,-8,-8,-8,-9,-9,-10,-10,-10,-11,-11,-12,-12,-13,-13,-14,-14,-15,-15,-15,-16,-16,-17,-17,-18,-18,-18,-19,-19,-20,-20,-20,-21,-21,-21,-21,-22,-22,-22,-22,-22,-22,-23,-23,-23,-23,-23,-23,-23,-22,-22,-22,-22,-22,-21,-21,-21,-20,-20,-19,-19,-18,-18,-17,-16,-16,-15,-14,-13,-13,-12,-11,-10,-9,-8,-7,-6,-5,-3,-2,-1,0,1,2,4,5,6,8,9,10,11,13,14,15,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,33,34,35,36,36,37,38,38,39,39,39,39,40,40,40,40,39,39,39,38,38,37,37,36,35,34,33,32,31,29,28,27,25,23,22,20,18,16,14,12,10,8,5,3,1,-2,-4,-7,-9,-12,-15,-17,-20,-23,-26,-28,-31,-34,-37,-39,-42,-45,-47,-50,-53,-55,-58,-60,-63,-65,-67,-69,-71,-73,-75,-77,-78,-80,-81,-83,-84,-85,-86,-86,-87,-87,-87,-87,-87,-87,-86,-86,-85,-84,-82,-81,-79,-77,-75,-73,-70,-68,-65,-61,-58,-54,-51,-47,-42,-38,-33,-28,-23,-18,-12,-7,-1,5,12,18,25,32,38,46,53,60,68,76,84,92,100,108,116,125,133,142,150,159,168,176,185,194,203,212,220,229,238,247,255,264,272,281,289,297,306,314,322,329,337,344,352,359,366,373,379,385,392,397,403,409,414,419,423,428,432,436,439,443,446,449,451,453,455,456,458,459,459,460,460,459,459,458,456,455,453,451,449,446,443,439,436,432,428,423,419,414,409,403,397,392,385,379,373,366,359,352,344,337,329,322,314,306,297,289,281,272,264,255,247,238,229,220,212,203,194,185,176,168,159,150,142,133,125,116,108,100,92,84,76,68,60,53,46,38,32,25,18,12,5,-1,-7,-12,-18,-23,-28,-33,-38,-42,-47,-51,-54,-58,-61,-65,-68,-70,-73,-75,-77,-79,-81,-82,-84,-85,-86,-86,-87,-87,-87,-87,-87,-87,-86,-86,-85,-84,-83,-81,-80,-78,-77,-75,-73,-71,-69,-67,-65,-63,-60,-58,-55,-53,-50,-47,-45,-42,-39,-37,-34,-31,-28,-26,-23,-20,-17,-15,-12,-9,-7,-4,-2,1,3,5,8,10,12,14,16,18,20,22,23,25,27,28,29,31,32,33,34,35,36,37,37,38,38,39,39,39,40,40,40,40,39,39,39,39,38,38,37,36,36,35,34,33,33,32,31,30,29,27,26,25,24,23,22,20,19,18,17,15,14,13,11,10,9,8,6,5,4,2,1,0,-1,-2,-3,-5,-6,-7,-8,-9,-10,-11,-12,-13,-13,-14,-15,-16,-16,-17,-18,-18,-19,-19,-20,-20,-21,-21,-21,-22,-22,-22,-22,-22,-23,-23,-23,-23,-23,-23,-23,-22,-22,-22,-22,-22,-22,-21,-21,-21,-21,-20,-20,-20,-19,-19,-18,-18,-18,-17,-17,-16,-16,-15,-15,-15,-14,-14,-13,-13,-12,-12,-11,-11,-10,-10,-10,-9,-9,-8,-8,-8,-7,-7,-6,-6,-6,-5,-5,-5,-4,-4,-4,-3,-3,-3,-3,-2,-2,-2,-2,-2,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,3
+// chanpats: 173
+// name: fir_compiler_1
+// filter_type: 2
+// rate_change: 0
+// interp_rate: 1
+// decim_rate: 64
+// zero_pack_factor: 1
+// coeff_padding: 22
+// num_coeffs: 724
+// coeff_sets: 1
+// reloadable: 0
+// is_halfband: 0
+// quantization: 0
+// coeff_width: 16
+// coeff_fract_width: 0
+// chan_seq: 0
+// num_channels: 1
+// num_paths: 2
+// data_width: 32
+// data_fract_width: 0
+// output_rounding_mode: 0
+// output_width: 48
+// output_fract_width: 0
+// config_method: 0
+
+const double fir_compiler_1_coefficients[724] = {3,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-3,-3,-3,-3,-4,-4,-4,-5,-5,-5,-6,-6,-6,-7,-7,-8,-8,-8,-9,-9,-10,-10,-10,-11,-11,-12,-12,-13,-13,-14,-14,-15,-15,-15,-16,-16,-17,-17,-18,-18,-18,-19,-19,-20,-20,-20,-21,-21,-21,-21,-22,-22,-22,-22,-22,-22,-23,-23,-23,-23,-23,-23,-23,-22,-22,-22,-22,-22,-21,-21,-21,-20,-20,-19,-19,-18,-18,-17,-16,-16,-15,-14,-13,-13,-12,-11,-10,-9,-8,-7,-6,-5,-3,-2,-1,0,1,2,4,5,6,8,9,10,11,13,14,15,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,33,34,35,36,36,37,38,38,39,39,39,39,40,40,40,40,39,39,39,38,38,37,37,36,35,34,33,32,31,29,28,27,25,23,22,20,18,16,14,12,10,8,5,3,1,-2,-4,-7,-9,-12,-15,-17,-20,-23,-26,-28,-31,-34,-37,-39,-42,-45,-47,-50,-53,-55,-58,-60,-63,-65,-67,-69,-71,-73,-75,-77,-78,-80,-81,-83,-84,-85,-86,-86,-87,-87,-87,-87,-87,-87,-86,-86,-85,-84,-82,-81,-79,-77,-75,-73,-70,-68,-65,-61,-58,-54,-51,-47,-42,-38,-33,-28,-23,-18,-12,-7,-1,5,12,18,25,32,38,46,53,60,68,76,84,92,100,108,116,125,133,142,150,159,168,176,185,194,203,212,220,229,238,247,255,264,272,281,289,297,306,314,322,329,337,344,352,359,366,373,379,385,392,397,403,409,414,419,423,428,432,436,439,443,446,449,451,453,455,456,458,459,459,460,460,459,459,458,456,455,453,451,449,446,443,439,436,432,428,423,419,414,409,403,397,392,385,379,373,366,359,352,344,337,329,322,314,306,297,289,281,272,264,255,247,238,229,220,212,203,194,185,176,168,159,150,142,133,125,116,108,100,92,84,76,68,60,53,46,38,32,25,18,12,5,-1,-7,-12,-18,-23,-28,-33,-38,-42,-47,-51,-54,-58,-61,-65,-68,-70,-73,-75,-77,-79,-81,-82,-84,-85,-86,-86,-87,-87,-87,-87,-87,-87,-86,-86,-85,-84,-83,-81,-80,-78,-77,-75,-73,-71,-69,-67,-65,-63,-60,-58,-55,-53,-50,-47,-45,-42,-39,-37,-34,-31,-28,-26,-23,-20,-17,-15,-12,-9,-7,-4,-2,1,3,5,8,10,12,14,16,18,20,22,23,25,27,28,29,31,32,33,34,35,36,37,37,38,38,39,39,39,40,40,40,40,39,39,39,39,38,38,37,36,36,35,34,33,33,32,31,30,29,27,26,25,24,23,22,20,19,18,17,15,14,13,11,10,9,8,6,5,4,2,1,0,-1,-2,-3,-5,-6,-7,-8,-9,-10,-11,-12,-13,-13,-14,-15,-16,-16,-17,-18,-18,-19,-19,-20,-20,-21,-21,-21,-22,-22,-22,-22,-22,-23,-23,-23,-23,-23,-23,-23,-22,-22,-22,-22,-22,-22,-21,-21,-21,-21,-20,-20,-20,-19,-19,-18,-18,-18,-17,-17,-16,-16,-15,-15,-15,-14,-14,-13,-13,-12,-12,-11,-11,-10,-10,-10,-9,-9,-8,-8,-8,-7,-7,-6,-6,-6,-5,-5,-5,-4,-4,-4,-3,-3,-3,-3,-2,-2,-2,-2,-2,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,3};
+
+const xip_fir_v7_2_pattern fir_compiler_1_chanpats[1] = {P_BASIC};
+
+static xip_fir_v7_2_config gen_fir_compiler_1_config() {
+  xip_fir_v7_2_config config;
+  config.name                = "fir_compiler_1";
+  config.filter_type         = 2;
+  config.rate_change         = XIP_FIR_INTEGER_RATE;
+  config.interp_rate         = 1;
+  config.decim_rate          = 64;
+  config.zero_pack_factor    = 1;
+  config.coeff               = &fir_compiler_1_coefficients[0];
+  config.coeff_padding       = 22;
+  config.num_coeffs          = 724;
+  config.coeff_sets          = 1;
+  config.reloadable          = 0;
+  config.is_halfband         = 0;
+  config.quantization        = XIP_FIR_INTEGER_COEFF;
+  config.coeff_width         = 16;
+  config.coeff_fract_width   = 0;
+  config.chan_seq            = XIP_FIR_BASIC_CHAN_SEQ;
+  config.num_channels        = 1;
+  config.init_pattern        = fir_compiler_1_chanpats[0];
+  config.num_paths           = 2;
+  config.data_width          = 32;
+  config.data_fract_width    = 0;
+  config.output_rounding_mode= XIP_FIR_FULL_PRECISION;
+  config.output_width        = 48;
+  config.output_fract_width  = 0,
+  config.config_method       = XIP_FIR_CONFIG_SINGLE;
+  return config;
+}
+
+const xip_fir_v7_2_config fir_compiler_1_config = gen_fir_compiler_1_config();
+
---
+++ system.gen/sources_1/ip/fir_compiler_1/fir_compiler_1_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+-- Date        : Mon Apr  3 18:46:26 2023
+-- Host        : TINNGUYEN-AMD-PC running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               e:/Documents/vivado_2022_proj/radio_periph_lab/system.gen/sources_1/ip/fir_compiler_1/fir_compiler_1_stub.vhdl
+-- Design      : fir_compiler_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fir_compiler_1 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_data_tvalid : in STD_LOGIC;
+    s_axis_data_tready : out STD_LOGIC;
+    s_axis_data_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 95 downto 0 )
+  );
+
+end fir_compiler_1;
+
+architecture stub of fir_compiler_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_data_tvalid,s_axis_data_tready,s_axis_data_tdata[63:0],m_axis_data_tvalid,m_axis_data_tdata[95:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fir_compiler_v7_2_18,Vivado 2022.1";
+begin
+end;
